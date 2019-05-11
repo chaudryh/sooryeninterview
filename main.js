3b61d49fe71755c229162bd6fda037e33d0e45b2
Hasana Chaudry <hasanachaudry@gmail.com>
2019-05-11 01:23:31 -0400

    commit #1

--- main.js
+++ main.js
@@ -2,21 +2,42 @@
 
 // 1. Make each item's paragraph collapse/expand when the title is clicked
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-
-
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
 
 // 2. Remove each item when we click on the closing X
 
-
-
-
+$(document).on('click', '.glyphicon-remove-circle', function () {
+    $(this).parents('.panel').get(0).remove();
+});
 
 // 3. Create a new item using the input field
 //    The title of the new item has to match the input value
 //    The new item should also have the same properties (collapse/expand and close) as the other items
 
+var $template = $(".template");
 
+var num = 2;
+$(".add").on("click", function () {
+    var $newPanel = $template.clone();
+    $newPanel.find(".collapse").removeClass("item");
+    $newPanel.find(".accordion-toggle").attr("href", "#" + (++num))
+        .text("Title #" + hash);
+    $newPanel.find(".panel-collapse").attr("id", num);
+    $("#accordion").append($newPanel.fadeIn());
+});
 
 
 //    Optional: 
